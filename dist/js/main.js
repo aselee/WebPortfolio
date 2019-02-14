b38f802abb6b7a32f8467343c63ac2e8041556d4
Andrew <32229265+aselee@users.noreply.github.com>
2019-02-13 18:27:10 -0600

    menu btn js & css transform

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,11 @@
 // select DOM items
 
-const menuButton = document.querySelector('.btn-menu');
+const menuButton = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 
-const navItems = document.querySelectorAll('.nav-items');
+const navItem = document.querySelectorAll('.nav-item');
 
 // setting the initial state of the menu
 
@@ -14,9 +14,31 @@ let showMenu = false;
 // need an eventlistener
 menuButton.addEventListener('click', toggleMenu);
 
+// want to show the classes as well as closed classes
+
 function toggleMenu() {
   if(!showMenu) {
+    menuButton.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
 
+    navItem.forEach(item => item.classList.add('show'));
+
+  // Set Menu State
+    showMenu = true;
+
+  // removing classes
+  } else {
+    menuButton.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+
+    navItem.forEach(item => item.classList.remove('show'));
+
+    // Set Menu State
+    showMenu = true;
   }
 }
 
